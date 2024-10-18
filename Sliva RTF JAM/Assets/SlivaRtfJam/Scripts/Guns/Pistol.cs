61b9ae6b93c5db18fcb855755c4cf01382235181
TyulenOl <setu787898@gmail.com>
2024-10-18 12:26:31 +0500

    Automatic shooting

--- Sliva RTF JAM/Assets/SlivaRtfJam/Scripts/Guns/Pistol.cs
+++ Sliva RTF JAM/Assets/SlivaRtfJam/Scripts/Guns/Pistol.cs
@@ -1,7 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace SlivaRtfJam.Scripts.Guns
 {
     public class Pistol : Gun
     {
-        
+        [SerializeField] private bool isAutomatigShooting;
+
+        private bool isShootPerformed;
+        private bool isShootCanceled;
+
+        protected override void Update()
+        {
+            if (remainingShootingDelay >= 0)
+            {
+                remainingShootingDelay -= Time.deltaTime;
+            }
+            
+            if (isShooting)
+            {
+                if (remainingShootingDelay <= 0 && currentAmmoInMag > 0 && isAutomatigShooting)
+                {
+                    MakeShoot();
+                }
+            }
+        }
+
+        public override void OnShoot(InputAction.CallbackContext context)
+        {
+            if(!isActiveAndEnabled)
+            {
+                return;
+            }
+
+            isShooting = context.performed;
+            
+            if (isShooting && remainingShootingDelay <= 0 && currentAmmoInMag > 0 && !isAutomatigShooting)
+            {
+                MakeShoot();
+            }
+        }
     }
 }
